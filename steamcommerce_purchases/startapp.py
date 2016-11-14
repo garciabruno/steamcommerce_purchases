4f34c453e72ba56588f694c53ed64f4329e35101
Bruno <garcia.bruno0@gmail.com>
2016-11-14 08:56:10 -0300

    Added threaded start

--- steamcommerce_purchases/startapp.py
+++ steamcommerce_purchases/startapp.py
@@ -7,6 +7,11 @@ import config
 if __name__ == "__main__":
     import sys
     sys.path.insert(0, '..')
-    __import__('app').app.run(debug=config.DEBUG, host=config.HOST)
+    __import__('app').app.run(
+        debug=config.DEBUG,
+        host=config.HOST,
+        port=config.PORT,
+        threaded=True
+    )
 else:
     raise RuntimeError('%s not meant for import' % __name__)
