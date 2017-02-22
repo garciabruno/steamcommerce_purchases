db5618ca189bf31fdfe33ebe9d59c07526f137b1
Bruno <garcia.bruno0@gmail.com>
2017-02-22 12:44:59 -0300

    Readibility

--- core/items.py
+++ core/items.py
@@ -19,7 +19,8 @@ class SteamCart(demiurge.Item):
     count = demiurge.TextField(selector='span#cart_item_count_value')
     subtotal = demiurge.TextField(selector='div#cart_price_total')
     balance = demiurge.TextField(selector='a#header_wallet_balance')
-    items = demiurge.RelatedItem(SteamCartItem, selector='div.cart_item_list')
 
     cart_status_message = demiurge.TextField(selector='div.cart_status_message')
     cart_checkout_button = demiurge.AttributeValueField(selector='a.continue:eq(0)', attr='href')
+
+    items = demiurge.RelatedItem(SteamCartItem, selector='div.cart_item_list')
