9d812fc9deec19a647b663d9e5627a175bb46500
Bruno <garcia.bruno0@gmail.com>
2017-03-01 18:03:11 -0300

    Added endpoint to get transaction link, checkout cart and get task status. Fixed healthcheck endpoint not returning floats

--- blueprints/edge.py
+++ blueprints/edge.py
@@ -20,11 +20,32 @@ def edge_healthcheck():
     now = time.time()
 
     try:
-        diff = int(now - float(requested_at))
+        diff = now - float(requested_at)
     except ValueError:
         diff = 0
 
     return str(diff)
+
+
+@edge.route('/task/state/', methods=['POST'])
+@route_decorators.as_json
+def edge_cart_status():
+    task_id = request.form.get('task_id')
+    task_name = request.form.get('task_name')
+
+    if not getattr(edge_task, task_name):
+        return {
+            'success': False,
+            'result': enums.EdgeResult.TaskNotFound.value
+        }
+
+    task = getattr(edge_task, task_name).AsyncResult(task_id)
+
+    return {
+        'success': True,
+        'task_status': task.state,
+        'task_result': task.result
+    }
 
 
 @edge.route('/cart/push/', methods=['POST'])
@@ -58,28 +79,63 @@ def edge_cart_push():
     task = edge_task.add_subids_to_cart.delay(network_id, items)
 
     return {
+        'success': True,
         'task_id': task.id,
         'task_status': task.status,
         'task_name': 'add_subids_to_cart'
     }
 
 
-@edge.route('/task/state/', methods=['POST'])
+@edge.route('/cart/checkout/', methods=['POST'])
 @route_decorators.as_json
-def edge_cart_status():
-    task_id = request.form.get('task_id')
-    task_name = request.form.get('task_name')
+def edge_cart_checkout():
+    network_id = request.form.get('network_id')
+    giftee_account_id = request.form.get('giftee_account_id')
+    payment_method = request.form.get('payment_method') or 'steamaccount'
 
-    if not getattr(edge_task, task_name):
+    try:
+        network_id = int(network_id)
+    except ValueError:
         return {
             'success': False,
-            'result': enums.EdgeResult.TaskNotFound.value
+            'result': enums.EdgeResult.ParamNotSerializable.value
         }
 
-    task = getattr(edge_task, task_name).AsyncResult(task_id)
+    task = edge_task.checkout_cart.delay(network_id, giftee_account_id, payment_method=payment_method)
 
     return {
         'success': True,
-        'task_status': task.state,
-        'task_result': task.result
+        'task_id': task.id,
+        'task_status': task.status,
+        'task_name': 'checkout_cart'
+    }
+
+
+@edge.route('/transaction/link/', methods=['POST'])
+@route_decorators.as_json
+def edge_transaction_link():
+    transid = request.form.get('transid')
+    network_id = request.form.get('network_id')
+
+    if not transid:
+        return {
+            'succes': False,
+            'result': enums.EdgeResult.IncompleteForm.value
+        }
+
+    try:
+        network_id = int(network_id)
+    except ValueError:
+        return {
+            'success': False,
+            'result': enums.EdgeResult.ParamNotSerializable.value
+        }
+
+    task = edge_task.get_external_link_from_transid.delay(network_id, transid)
+
+    return {
+        'success': True,
+        'task_id': task.id,
+        'task_status': task.status,
+        'task_name': 'get_external_link_from_transid'
     }
